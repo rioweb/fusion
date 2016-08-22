70f8c34d4ab406a3d6d611253dfffdf097e8dd73
Luis <luisfg25>
2016-08-22 12:27:42 -0300

    primer commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,19 @@
+'use strict';
+var main = function(){
+  /*global someFunction Waypoint:true*/
+/*eslint no-undef: "error"*/
+
+/*eslint no-unused-vars: "error"*/
+  /* global $ */
+$('.nav li, .nav li a').click(function (e) {
+		e.preventDefault();
+		$('ul.nav > li').removeClass('active');
+            $(this).addClass('active');
+
+});
+
 $(document).ready(function(){
       $('.parallax').parallax();
     });
-        
+    
+$(document).ready(main);
